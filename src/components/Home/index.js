3c50f3f8af57c842e6e33e7dcd5b03a372c0f9a1
Tveerababu <babuv9985@gamil.com>
2023-05-23 08:59:11 +0000

    vajsv

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import LogoutButton from '../LogoutButton'
+
+import './index.css'
+
+const About = () => (
+  <>
+    <Header />
+    <div className="about-container">
+      <h1>Home Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+
+export default About
